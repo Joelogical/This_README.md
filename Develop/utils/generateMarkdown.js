5a1af66f168aa3158e159cb000143acf2571ad58
Joelogical <jmsmith.coding@gmail.com>
2024-12-05 21:51:07 -0800

    final touches

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,4 +1,4 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
   if (!license) return "";
@@ -8,7 +8,7 @@ function renderLicenseBadge(license) {
   )}-blue.svg)`;
 }
 
-// TODO: Create a function that returns the license link
+// Function that returns the license link
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (!license) return ""; // Return empty string if no license is provided
@@ -24,7 +24,7 @@ function renderLicenseLink(license) {
   return ""; // Return empty string if no valid license is matched
 }
 
-// TODO: Create a function that returns the license section of README
+// Function that returns the license section of README
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   const badge = renderLicenseBadge(license);
@@ -39,7 +39,7 @@ function renderLicenseSection(license) {
   `;
 }
 
-// TODO: Create a function to generate markdown for README
+// Function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
